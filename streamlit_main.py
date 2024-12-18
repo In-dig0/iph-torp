abd6d3b8514e75c1e2e9bb8b6155748c053591dc
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2024-12-18 22:39:12 +0100

    update app

--- streamlit_main.py
+++ streamlit_main.py
@@ -180,7 +180,7 @@ def check_request_fields(record: dict) -> bool:
     res = True
     mandatory_fields = ["Req_dept", "Req_requester", "Prd_line", "Prd_family"]
     for k in list(record.keys()):
-        if k in mandatory_fields and record[k] = "":
+        if k in mandatory_fields and record[k] == "":
             res = False
     return res
 

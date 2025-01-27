843398850dadb75987b98cc7b6e1bcc61937d4d3
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-01-27 09:04:48 +0100

    Create module_display_app_info.py

---
+++ module_display_app_info.py
@@ -0,0 +1,14 @@
+import streamlit
+
+def display_app_info():
+    """ Show app title and description """
+    
+    st.header(":blue[TORP Web Application]", divider="blue")
+    st.subheader(
+        """
+        TORP - Technical Office Requests POC (Proof Of Concept), is a simple web application to manage requests to IPH Technical Office.
+        """
+    )
+    st.markdown(f":grey[Version: {APPVERSION}]")
+    st.divider()
+    st.markdown("Powered with Streamlit :streamlit:")

dbf6e429d213ce305085eb52cfae834ac1a2200a
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-01-27 09:01:28 +0100

    Update mytest.py

--- mytest.py
+++ mytest.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
 def my_module_test():
-  print(f"Test loaded from module mytest.py")
-  st.write(f"test completato con successo!!")
+  st.success(f"test completato con successo!!")
 
-print(f"Module mytest.py imported")  

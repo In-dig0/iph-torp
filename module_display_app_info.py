7fdc47f0ef0c2de3fdbe781d17e53180b5c0ee36
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-01-27 09:22:34 +0100

    Update module_display_app_info.py

--- module_display_app_info.py
+++ module_display_app_info.py
@@ -1,11 +1,7 @@
 import streamlit as st
 
-# Global variables
-APPNAME = "TORP" #IPH Technical Office Request POC (Proof Of Concept)
-APPCODE = "TORP"
-APPVERSION = "0.2"
 
-def display_app_info() -> None:
+def display_app_info(appname: str, appcode: str, appversion: str) -> None:
     """ Show app title and description """
     
     st.header(":blue[TORP Web Application]", divider="blue")
@@ -14,6 +10,6 @@ def display_app_info() -> None:
         TORP - Technical Office Requests POC (Proof Of Concept), is a simple web application to manage requests to IPH Technical Office.
         """
     )
-    st.markdown(f":grey[Version: {APPVERSION}]")
+    st.markdown(f":grey[Version: {appversion}]")
     st.divider()
     st.markdown("Powered with Streamlit :streamlit:")

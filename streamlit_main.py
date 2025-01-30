f6798637c650d69484a822d8636745cfe2ed3eca
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-01-30 10:11:41 +0100

    upd app

--- streamlit_main.py
+++ streamlit_main.py
@@ -1879,7 +1879,7 @@ def manage_wo():
     sorted_usernames = sorted(unique_usernames)
     wo_username_options = list(sorted_usernames)
     
-    selected_username = st.sidebar.selectbox(label="TD user:", options=wo_username_options, index=None)
+    selected_username = st.sidebar.selectbox(label="Tech Deparment Specialist:", options=wo_username_options, index=None)
     
     df_wo_usercode = df_woassignedto[df_woassignedto['USERNAME'] == selected_username]["USERID"].unique()
     wo_usercode = list(df_wo_usercode)

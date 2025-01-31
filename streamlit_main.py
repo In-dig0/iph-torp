7582dc478c664f42b141219df2592b6323c8ad24
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-01-31 12:45:25 +0100

    upd app

--- streamlit_main.py
+++ streamlit_main.py
@@ -2004,6 +2004,7 @@ def manage_wo():
             wi_save_botton_disable = not (wi_task_l1 and wi_task_l2 and wi_description and wi_time_qty and wi_date)
             #st.write(f"'{wi_task_l1}' - '{wi_task_l2}' - '{wi_description}' - {wi_time_qty}' - {wi_date}'")
             wi_save_botton_disable = False
+            submitted = False
             submitted = st.form_submit_button("Save Work Item", disabled=wi_save_botton_disable)
 
             if submitted: 

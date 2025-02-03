522031b7ecc0f02e2c44e5404a5f4f00bf134408
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-03 12:00:16 +0100

    upd app

--- insert_workitem.py
+++ insert_workitem.py
@@ -62,10 +62,10 @@ def insert_workitems(conn):
 
     # Calcola la data di oggi meno 7 giorni
     previus_7days = datetime.datetime.now() - datetime.timedelta(days=7)
-    selected_from_date = st.sidebar.date_input("From date", value=previus_7days, key="di_datefrom", format="DD/MM/YYYY", disabled=False)
-    selected_to_date = st.sidebar.date_input("To date", value="today", key="di_dateto", format="DD/MM/YYYY", disabled=False)
-    # # Stampa la data calcolata
-    # print("Oggi meno 7 giorni:", oggi_meno_7_giorni.strftime("%Y-%m-%d"))
+    selected_from_date = st.sidebar.date_input(":blue[From date]", value=previus_7days, key="di_datefrom", format="DD/MM/YYYY", disabled=False)
+    selected_to_date = st.sidebar.date_input(":blue[To date]", value="today", key="di_dateto", format="DD/MM/YYYY", disabled=False)
+    st.sidebar.button("Search", key="search_button", type="primary", use_container_width=True, on_click=None)
+
 
 
         

e8c72ee8fdd06f51dcdb362991f53e4e20528885
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-03 10:01:42 +0100

    upd app

--- view_request.py
+++ view_request.py
@@ -25,7 +25,7 @@ def view_request(conn) -> None:
         
         "Reload request data into df"
         st.session_state['df_requests'] = sqlite_db.load_requests_data
-        
+        st.write(st.session_state)
         # Lista delle chiavi di sessione da eliminare
         keys_to_clear = [
             'grid_data',

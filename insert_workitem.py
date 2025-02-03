16b4628fce5e4ec8aa0a951ba7fb3a458fc99324
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-03 13:07:43 +0100

    upd app

--- insert_workitem.py
+++ insert_workitem.py
@@ -67,11 +67,11 @@ def insert_workitems(conn):
             (st.session_state.df_workitems["DATE"] >= selected_from_date) & 
             (st.session_state.df_workitems["DATE"] <= selected_to_date)]
 
-        with st.container("Workitem grid", border=True, key="Workitem grid"):
+        with st.container(border=True, key="Workitem grid"):
             st.dataframe(data=filtered_workitems, use_container_width=True, hide_index=False)
         #st.dataframe(data=None, width=None, height=None, *, use_container_width=False, hide_index=None, column_order=None, column_config=None, key=None, on_select="ignore", selection_mode="multi-row")
         st.divider()
-        with st.container("Insert Task", border=True, key="Insert Task"):
+        with st.container(border=True, key="Insert Task"):
             st.subheader(f":orange[Task]")
 
             #with st.form(key='task_form'):

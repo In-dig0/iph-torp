633743d1f8c7fcffbb7b373e405e43a40e492970
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-04 15:07:24 +0100

    upd app

--- manage_request.py
+++ manage_request.py
@@ -391,7 +391,7 @@ def manage_request(conn):
             # Handle save action
             if st.button("Save", type="primary", disabled=disable_save_button, key="wo_save_button"):
                 wo = {"woid":woid, "tdtlid": req_tdtl_code, "type": wo_type, "title": selected_row["TITLE"][0], "description": req_description_default, "time_qty": wo_time_qty, "time_um": wo_time_um, "status": ACTIVE_STATUS, "startdate": wo_startdate, "enddate": wo_enddate, "reqid": reqid}
-                wo_idrow, success = sqlite_db.save_workorder(wo)
+                wo_idrow, success = sqlite_db.save_workorder(wo, conn)
                 if success:
 #                    st.success(f"Work order W{str(wo_idrow).zfill(4)} created successfully!")
                     success = sqlite_db.save_workorder_assignments(woid, wo_assignedto, df_users, df_woassignedto)

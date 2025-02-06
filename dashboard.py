6bcb4b53c9c4d2178889cff818828e4f917101d0
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-06 21:36:06 +0100

    upd app

--- dashboard.py
+++ dashboard.py
@@ -9,3 +9,44 @@ import servant
 
 def dashboard(conn):
     pass
+# Show some metrics and charts about the ticket.
+    st.header("Statistics")
+
+    # Show metrics side by side using `st.columns` and `st.metric`.
+    col1, col2, col3 = st.columns(3)
+    num_open_requests = len(st.session_state.df_requests[st.session_state.df_requests == "NEW"])
+    num_assigned_requests = len(st.session_state.df_requests[st.session_state.df_requests == "ASSIGNED"])
+    num_completed_requests = len(st.session_state.df_requests[st.session_state.df_requests == "COMPLETED"])
+    col1.metric(label="Number of open requests", value=num_open_requests, delta=10)
+    col2.metric(label="Number of assigned requests", value=num_assigned_requests, delta=-1.5)
+    col3.metric(label="Number of completed requests", value=num_completed_requests, delta=2)
+
+# Show two Altair charts using `st.altair_chart`.
+st.write("")
+st.write("##### Requests status per day")
+status_plot = (
+    alt.Chart(df_requests)
+    .mark_bar()
+    .encode(
+        x="month(Insdate):O",
+        y="count():Q",
+        xOffset="Status:N",
+        color="Status:N",
+    )
+    .configure_legend(
+        orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
+    )
+)
+st.altair_chart(status_plot, use_container_width=True, theme="streamlit")
+
+st.write("##### Current requests procut line")
+priority_plot = (
+    alt.Chart(df_requests)
+    .mark_arc()
+    .encode(theta="count():Q", color="PR_LINE:N")
+    .properties(height=300)
+    .configure_legend(
+        orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
+    )
+)
+st.altair_chart(priority_plot, use_container_width=True, theme="streamlit")

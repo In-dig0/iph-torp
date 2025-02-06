075ad05659471fb4f3a320fbdd1786169f757aa9
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-06 14:25:08 +0100

    upd app

--- manage_workorder.py
+++ manage_workorder.py
@@ -294,7 +294,7 @@ def manage_workorder(conn):
             if has_selection:
                 st.write(selected_rows)
                 #selected_row_dict = selected_rows.to_dict()
-                workorder_id = str(workorder_id.iloc[0]) 
+                workorder_id = str(selected_rows.iloc[0]) 
                 st.write(f"{type(workorder_id)} - {workorder_id}")
                 #success = create_work_item(workorder_id)
 

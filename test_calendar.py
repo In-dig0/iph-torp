016d4a0fbd72b15fb5fe99ad6775d1c640343266
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-12 11:07:12 +0100

    upd app

--- test_calendar.py
+++ test_calendar.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_calendar import calendar
 import calendar
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def show_calendar(conn):

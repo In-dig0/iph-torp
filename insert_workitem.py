3d3078124e2682195a5731f7afa4c210a03cc45c
Riccardo Baravelli <143281431+In-dig0@users.noreply.github.com>
2025-02-13 11:04:37 +0100

    upd app

--- insert_workitem.py
+++ insert_workitem.py
@@ -114,14 +114,22 @@ def create_workitem(conn)-> None:
             "title": f'[W25-0012] Update Scania project-> {time_wo1}',
             "start": '2025-02-12',
             "backgroundColor": '#d4efdf',
-            "borderColor": '#a2d9ce'
+            "borderColor": '#a2d9ce',
+            "extendedProps": 
+                {
+                "duration": '4h'
+                }
             },
             {
             "id": 'W25-0017',
             "title": f'[W25-0017] Update Volvo project-> {time_wo2}',
             "start": '2025-02-12',
             "backgroundColor": '#d4efdf',
-            "borderColor": '#a2d9ce'       
+            "borderColor": '#a2d9ce',
+            "extendedProps": 
+                {
+                "duration": '1.5h'
+                } 
             }
         ]
         
